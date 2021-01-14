93af7fecb20840b63deb57c7ee6db838534c8bec
Vitalii <22921785+sliusar@users.noreply.github.com>
2021-01-14 17:12:02 +0100

    Updating ignore

--- src/main.cu
+++ src/main.cu
@@ -14,7 +14,7 @@ int main(void)
   int N = 1<<20;
   float *x, *y;
   std::cout << N << std::endl;
-  // Allocate Unified Memory – accessible from CPU or GPU
+  // Allocate Unified Memory accessible from CPU or GPU
   cudaMallocManaged(&x, N*sizeof(float));
   cudaMallocManaged(&y, N*sizeof(float));
 
