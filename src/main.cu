e5c709c76ad7f96cf6b4a48309c7802a57c7a21b
Vitalii <22921785+sliusar@users.noreply.github.com>
2021-11-16 16:49:28 +0100

    Adding GPU info printout

--- src/main.cu
+++ src/main.cu
@@ -70,6 +70,34 @@ int write_lc(char* filename, float* lc, Configuration c) {
   return 0;
 }
 
+void DisplayGPUInfo() {
+    const int kb = 1024;
+    const int mb = kb * kb;
+
+    cout << "CUDA version:   v" << CUDART_VERSION << endl;    
+
+    int devCount;
+    cudaGetDeviceCount(&devCount);
+    cout << "CUDA Devices: " << endl << endl;
+
+    for(int i = 0; i < devCount; ++i)
+    {
+        cudaDeviceProp props;
+        cudaGetDeviceProperties(&props, i);
+        cout << i << ": " << props.name << ": " << props.major << "." << props.minor << endl;
+        cout << "  Global memory:   " << props.totalGlobalMem / mb << "mb" << endl;
+        cout << "  Shared memory:   " << props.sharedMemPerBlock / kb << "kb" << endl;
+        cout << "  Constant memory: " << props.totalConstMem / kb << "kb" << endl;
+        cout << "  Block registers: " << props.regsPerBlock << endl << endl;
+
+        cout << "  Warp size:         " << props.warpSize << endl;
+        cout << "  Threads per block: " << props.maxThreadsPerBlock << endl;
+        cout << "  Max block dimensions: [ " << props.maxThreadsDim[0] << ", " << props.maxThreadsDim[1]  << ", " << props.maxThreadsDim[2] << " ]" << endl;
+        cout << "  Max grid dimensions:  [ " << props.maxGridSize[0] << ", " << props.maxGridSize[1]  << ", " << props.maxGridSize[2] << " ]" << endl;
+        cout << endl;
+    }
+}
+
 double getCurrentTimestamp() {
   struct timeval time_now;
   gettimeofday(&time_now, nullptr);
@@ -98,6 +126,8 @@ int main(const int argc, const char** argv) {
     cerr << "Usage:\n\t" << argv[0] << " configuration.yaml" << endl;
     return 1;
   }
+
+  DisplayGPUInfo();
 
   char filename[64], output_folder[64];
 
